fcb39ef306b784f0b5fecfabd2233f4905c0de5e
ryanoliveiragit <ryanoliveirabrito178@gmail.com>
2026-05-08 10:41:40 -0300

    fix: use functions format with api/index.py for correct Python build

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "backend"))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "backend"))
 
 from main import app
